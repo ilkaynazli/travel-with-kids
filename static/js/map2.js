479c8553d329d0a5a70a2c388e015efeafbeb8ca
Ilkay Celik Gulsoy <ilkayncelik@gmail.com>
2018-10-13 00:44:36 +0000

    can show info window now

--- static/js/map2.js
+++ static/js/map2.js
@@ -1,15 +1,9 @@
 "use strict"
-const coordinates = [];
-let steps;
-let map;
-let marker;
-const markers = [];
-const contentString = new Map();
 function myCallBack(){
-    const route = $("#map").data()
-    const start = route['start'];
-    const end = route['end'];
+    const route = $("#map").data()      //Get start and end address from user
+    let map;
 
+    //Create Map and call calculate and display route function
     function createMap() {
         map = new google.maps.Map(document.getElementById('map'), {
                 center: {lat: 37.38 , lng: -121.94 },
@@ -21,19 +15,24 @@ function myCallBack(){
         calculateAndDisplayRoute(directionsService, directionsDisplay);
     };
 
+    //Calculate and Display route
     function calculateAndDisplayRoute(directionsService, directionsDisplay) {
-        directionsService.route({
-            origin: start,
-            destination: end,
+        directionsService.route({           //start, end points and travel mode
+            origin: route['start'],
+            destination: route['end'],
             travelMode: 'DRIVING'
-        }, function (response, status){
-            if (status == 'OK') {
+        }, function (response, status){     //take response and status from API
+            if (status == 'OK') {           
                 directionsDisplay.setDirections(response);
-                console.log(response);
+                console.log(response);          //delete this line when everything starts working//
                 const myRoute = response;
-                steps = myRoute.routes[0].legs[0].steps;
-                let index = 0;
+                const steps = myRoute.routes[0].legs[0].steps;  //take each step of your route 
                 let totalDistance = 0;
+                const coordinates = [];
+                //go through each step and each array the steps have.
+                //calculate the distance between each lat_lngs and add it to the total distance
+                //when the total distance is > 40000 meters add the final lat_lng to the coordinates array
+                //then 0 the total distance and continue until you reach the end point
                 for (let i=0; i < steps.length; i++){
                     for (let j=0; j < (steps[i].lat_lngs.length-1); j++) {
                         let distance = google.maps.geometry.spherical.computeDistanceBetween(steps[i].lat_lngs[j], steps[i].lat_lngs[j+1]);
@@ -47,12 +46,14 @@ function myCallBack(){
                         }
                     }  
                 };
-                const myJSON = JSON.stringify(coordinates);
+
                 const formInputs = {
-                    'myJSON': myJSON
+                    'myJSON': JSON.stringify(coordinates)
                 };
 
-                let infowindow = new google.maps.InfoWindow(); 
+                let marker;
+                let infoWindow = new google.maps.InfoWindow();
+                let myContent;
 
                 function show_playgrounds(results) {
                     const playgroundDetails = results;
@@ -64,9 +65,10 @@ function myCallBack(){
                         marker = new google.maps.Marker({
                             position: latLng,
                             map: map, 
-                            name : name                           
+                            title : name                           
                         });
-                        markers.push(marker);              
+                        myContent = '<div id="info-window">My Info Window</div>';  
+                        displayMyInfoWindow(marker, map,infoWindow, myContent);           
                     }
                 }
 
@@ -74,13 +76,14 @@ function myCallBack(){
                         formInputs, 
                         show_playgrounds)
                 
-                for (marker of markers) {
-                    $(marker).on('click', function() {
-                        infowindow.setContent('<div id="info-window">' + name + '</div>');
-                        infowindow.open(map, marker)                    
-                    });  
-                }        
+                function displayMyInfoWindow(marker, map, infoWindow, content) {
+                    google.maps.event.addListener(marker, 'click', function () {
+                        infoWindow.close();
+                        infoWindow.setContent(content);
+                        infoWindow.open(map, marker);
+                });
 
+            } 
             } else {
                 alert('Directions request failed due to ' + status);
             }
